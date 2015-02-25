522cef6503912a8ae7a7e064e37d2513121cbed0
Pooh <poorin_kong@cmu.ac.th>
2015-02-25 11:41:58 +0700

    6.1

--- src/main/java/camt/se331/shoppingcart/config/WebAppInitializer.java
+++ src/main/java/camt/se331/shoppingcart/config/WebAppInitializer.java
@@ -18,11 +18,22 @@ import javax.servlet.ServletRegistration.Dynamic;
 import java.io.IOException;
 
 public class WebAppInitializer implements WebApplicationInitializer {
-    
+    String basicConfig = "classpath:/embedSetup.propoties";
 	public void onStartup(ServletContext servletContext) throws ServletException {
 
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.getEnvironment().setActiveProfiles("datasource.remoteDB","db.dbdao");
+
+        //ctx.getEnvironment().setActiveProfiles("datasource.remoteDB","db.dbdao");
+
+        //Update
+        ConfigurableEnvironment env = ctx.getEnvironment();
+        try{
+            env.getPropertySources().addFirst(new ResourcePropertySource(basicConfig));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,env.getProperty("activeProfile"));
+        
         ctx.register(AppConfig.class);
         ctx.setServletContext(servletContext);    
         Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
